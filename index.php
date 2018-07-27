9f476292e3d80e2732ebb07c1f6f8bb14f7c73ef
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 14:38:26 +0200

    exo7p5 php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*Création tableau associatif departement*/
+  $departements = array(
+    /*Détermination des éléments clé => valeur*/
+    'Aisne' => 02,
+    'Nord' => 59,
+    'Oise' => 60,
+    'Pas-de-Calais' => 62,
+    'Somme' => 80);
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 7</title>
+  </head>
+  <body>
+<p>
+  <?php
+    $departements[51] = 'reims';
+    echo var_dump($departements);
+  ?>
+</p>
+  </body>
+</html>
